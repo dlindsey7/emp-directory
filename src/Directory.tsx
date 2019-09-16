7b9d2f631235b4f35ac6d8e444862b3f83816a1a
Dorothea Lindsey <dorothea.l.lindsey@gmail.com>
2019-09-16 15:21:41 -0400

    Cleanup and updated README

--- src/Directory.tsx
+++ src/Directory.tsx
@@ -2,85 +2,96 @@ import React, { Component } from 'react';
 import Employee from './Employee';
 
 class Directory extends Component<any, any> {
-  // Initialize the state
-  constructor(props: any){
-    super(props);
-    this.state = {
-      company: "TAZ Co",
-      allowModification: false
+    // Initialize the state
+    constructor(props: any){
+        super(props);
+        this.state = {
+        company: "TAZ Co",
+        allowModification: false,
+        isLoaded: false,
+        employees: []
+        }
     }
-  }
 
-  getEmployeeData = () => {
-    fetch("https://randomuser.me/api/?results=50")
-    .then( response => response.json())
-    .then(
-        // handle the result
-        (result) => {
-            this.setState({
-                isLoaded: true,
-                employees: result.results
-            });
-        },
-        // Handle error 
-        (error) => {
-            this.setState({
-                isLoaded: true,
-                error
-            })
-        },
-    )
-  };
+    getEmployeeData = () => {
+        // Mock employee data until there's data added to the backend
+        fetch("https://randomuser.me/api/?results=50")
+        .then( response => response.json())
+        .then(
+            // Set the app to loaded since the employee data is the most important
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    employees: result.results
+                });
+            },
+            // Set the error so that it can be displayed
+            (error) => {
+                this.setState({
+                    isLoaded: false,
+                    error
+                })
+            },
+        )
+    };
 
-  getCompanyData = () => {
-    fetch("http://localhost:3000/api/getCompany")
-    .then( response => response.json())
-    .then(
-        // handle the result
-        (result) => {
-            this.setState({
-                company: result.name,
-                allowModification: result.allowModification
-            });
-        },
-        // Handle error 
-        (error) => {
-            this.setState({
-                error
-            })
-        },
-    )
-  };
+    getCompanyData = () => {
+        // Hardcoded to work with local development backend
+        // TODO: Sort out cross domain error in production
+        fetch("http://localhost:3000/api/getCompany")
+        .then( response => response.json())
+        .then(
+            // Set company name and modification flag
+            (result) => {
+                this.setState({
+                    company: result.name,
+                    allowModification: result.allowModification
+                });
+            },
+            // Handle error here with grace so that the backend isn't 
+            // required to test the look of the frontend 
+            (error) => {
+                this.setState({
+                    company: "Placeholder"
+                })
+            },
+        )
+    };
 
-  componentDidMount(){
-    this.getEmployeeData();
-    this.getCompanyData();
-  }
-render() {
-    const {error, isLoaded, employees, company, allowModification} = this.state;
-    const style = allowModification ? {} : {display: 'none'};
-    if(error){
-        return <div>Error in loading</div>
-    }else if (!isLoaded) {
-        return <div>Loading ...</div>
-    }else{
-        return(
-            <div id="directory">
-                <h1>{company}</h1>
-                <button style={style}>Add Employee</button>
-                <button style={style}>Delete Employee</button>
-                {
-                Object.values(employees).map((emp: any, key: any) => (
-                    <div key={key}>
-                    <Employee employee={emp}/>
-                    </div>
-                ))
-                }
-            </div>
-        );
+    componentDidMount(){
+        this.getEmployeeData();
+        this.getCompanyData();
     }
-  
-}
+    render() {
+        const {error, isLoaded, employees, company, allowModification} = this.state;
+        // Only show add/delete buttons if the user is allowed to modify info
+        const style = allowModification ? {} : {display: 'none'};
+        if(error){
+            return <div>Error in loading</div>
+        }else if (!isLoaded) {
+            return <div>Loading ...</div>
+        }else{
+            return(
+                <div id="directory">
+                    <h1>{company}</h1>
+                    <button style={style}>Add Employee</button>
+                    <button style={style}>Delete Employee</button>
+                    { Object.keys(employees).length ? (
+                        // Get the values because we're starting with an array structure
+                        Object.values(employees).map((emp: any, key: any) => (
+                            <div key={key}>
+                                <Employee employee={emp}/>
+                            </div>
+                        ))
+                    ) : (
+                        <div>Missing employee information</div>
+                    )
+                    }
+                </div>
+            );
+        }
+    
+    }
 };
 
 export default Directory;
--- src/Employee.tsx
+++ src/Employee.tsx
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import icon from './spooky.jpg';
+import icon from './spooky.jpg'; // Placeholder employee icon
 
 interface EmployeeProps {
     employee: Object
@@ -16,16 +16,16 @@ class Employee extends Component<EmployeeProps, any> {
 
     render() {
         const {employee} = this.state;
-
+            // Check that the employee has information to show
             if (Object.keys(employee).length > 0) {
-
+                
                 return(
                     <div id="person">
                         <div id="name">
                             <img id="icon" src={employee.picture.thumbnail} alt={icon} height='56px' width='56px'/>
                             {employee.name.first} {employee.name.last}
                         </div>
-                        <div>Associate</div>
+                        <div>Adventurer</div>
                             <div id="info">{employee.email}</div>
                     </div>
                 );
